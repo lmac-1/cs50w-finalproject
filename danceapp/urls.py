7bf6bcb76fcfc7261b1dd263c13e9663db0d2de7
lmac-1 <macartney.lucy@gmail.com>
2021-07-29 10:03:22 -0500

    Logout + creating teacher account (dev only)

--- danceapp/urls.py
+++ danceapp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("register", views.register_student, name="register_student"),
     path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
     path("newvideo", views.new_video, name="new_video")
 ]
--- danceapp/views.py
+++ danceapp/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from django.db import IntegrityError
 from django.contrib import messages
 
-from .models import User, Student, Video
+from .models import User, Student, Video, Teacher
 from .forms import NewVideoForm
 
 def index(request):
@@ -57,6 +57,7 @@ def register_student(request):
         email = request.POST["email"]
         first_name = request.POST["first_name"]
         last_name = request.POST["last_name"]
+        account_type = request.POST["account_type"]
 
         # Ensure password matches confirmation
         password = request.POST["password"]
@@ -73,16 +74,31 @@ def register_student(request):
             # Saves first name, last name and student flag
             user.first_name = first_name
             user.last_name = last_name
-            user.is_student = True
+            
+            if account_type == "student":
+                user.is_student = True
+            else:
+                user.is_teacher = True
+            
             # Saves user
             user.save()
-            # Creates student instance of user
-            student = Student.objects.create(user=user)
+
+            if account_type == "student":
+                # Creates student instance of user
+                student = Student.objects.create(user=user)
+            else:
+                teacher = Teacher.objects.create(user=user)
 
         except IntegrityError:
             messages.error(request, 'Username already taken.')
             return HttpResponseRedirect(reverse("register_student"))
+
         login(request, user)
         return HttpResponseRedirect(reverse("index"))
+
     else:
         return render(request, "danceapp/register.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("login"))
