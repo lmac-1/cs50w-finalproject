2659b3297aa46e23de86e36f37dc1a443627532c
lmac-1 <macartney.lucy@gmail.com>
2021-08-05 18:06:32 -0500

    Complete rewrite of New Video JS

--- danceapp/static/danceapp/newvideo.js
+++ danceapp/static/danceapp/newvideo.js
@@ -1,49 +1,116 @@
-async function getVideoAndUpdatePage(youtubeId) {
-    const videoJsonData = await getVideoDataFromId(youtubeId);
-    const videoObject = await createVideoObject(videoJsonData);
-    updatePage(videoObject);
-}
+const apiKey = 'AIzaSyBPfi6PU5j6pvvAR3sexwu8APAm_IcfNZ4';
 
-// [TODO] Error handling
-async function getVideoDataFromId(youtubeId) {
-    const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?id=${youtubeId}&key=AIzaSyBPfi6PU5j6pvvAR3sexwu8APAm_IcfNZ4&part=snippet,contentDetails,statistics,status`);
-    const jsonVideoData = await response.json();
-    console.log(jsonVideoData);
-    return jsonVideoData;
-}
-
-async function createVideoObject(videoJsonData) {
+async function processYoutubeUrl(url) {
     
+    // SPLIT THE URL
+    let potentialIds = getPotentialIds(url);
+
+    if (potentialIds != null) {
+        
+        // Here we process the potential Ids to try to get an object with the video information
+        let videoData = await getValidVideoObject(potentialIds);
+        
+        if (videoData != null) {
+            // update the page
+            console.log(videoData)
+            updatePage(videoData);
+        }
+        else {
+            invalidId('The URL did not contain a valid ID');
+        }
+    }
+    else {
+        invalidId('The URL does not contain a parameter with 11 characters that could potentially be an ID');
+    }
+}
+
+function getPotentialIds(url) {
+    
+    // At the time of writing, YouTube IDs have 11 characters
+    let lengthOfValidYouTubeID = 11;
+    
+    // This separates the URL into parts using delimiters / . ? & and =
+    let urlParameters = url.split(/\/|\.|\?|&|=/);
+    
+    // We create an empty array to hold the potential Ids
+    let potentialIds = [];
+
+    // We iterate through the urlParameters checking for any with length 11, pushing them into potentialIds
+    for (const parameter of urlParameters) {
+        if (parameter.length === lengthOfValidYouTubeID) {
+            potentialIds.push(parameter);
+        }
+    }
+
+    if (potentialIds.length >= 1) return potentialIds;
+    else return null;
+}
+
+async function getValidVideoObject(ids) {
+
+    for (const id of ids) {
+        
+        // We try to call the YouTube API with the given id and convert to JSON
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?id=${id}&key=${apiKey}&part=snippet,contentDetails,statistics,status`);
+        const jsonResponse = await response.json();
+        console.log(jsonResponse);
+        
+        // Here we are checking if the ID is valid
+        if (jsonResponse.hasOwnProperty('pageInfo') && jsonResponse.pageInfo.totalResults === 1 && jsonResponse.items[0].kind==='youtube#video') {
+            // If the results give a youtube video, we will create the youtube object
+                return createVideoObject(jsonResponse);
+        }
+    }
+
+    return null;
+}
+
+function createVideoObject(videoJsonData) {
+
+    // This part of the JsonData holds all info about the youtube video
+    // We have previously verified that this is the only result in the JsonData
     let video = videoJsonData.items[0].snippet;
-    console.log(video);
-    
-    /* [TODO] Build a constructor for this type of object */
-    let videoTitle = video.title;
-    let videoDescription = video.description;
-    let videoImage = video.thumbnails.medium.url;
-    let videoId = videoJsonData.items[0].id;
 
-    // Returns video object which will be used to prepopulate fields
-    return {id: videoId, title: videoTitle, description: videoDescription, thumbnailUrl: videoImage};
+    /* [TODO] Build a constructor for this type of object */
+    // We pull the title, description, thumbnail url and id of the youtube video from the youtube API
+    let id = videoJsonData.items[0].id;
+    let title = video.title;
+    let description = video.description;
+    let thumbnailUrl = video.thumbnails.medium.url;
+
+    // Returns video object which will be used to prepopulate fields on page
+    return {
+            id, 
+            title, 
+            description, 
+            thumbnailUrl
+        };
 }
 
-function updatePage(videoObject) {
-    document.getElementById('videoTitle').value = videoObject.title;
-    document.getElementById('videoDescription').value = videoObject.description;
-    document.getElementById('videoThumbnailUrl').value = videoObject.thumbnailUrl;
-    document.getElementById('videoYoutubeId').value = videoObject.id;
+// this will hold our error handling for invalid ids / no response
+function invalidId(errorMessage) {
+    console.error(errorMessage);
+}
+
+function updatePage(videoData) {
+    document.getElementById('videoTitle').value = videoData.title;
+    document.getElementById('videoDescription').value = videoData.description;
+    document.getElementById('videoThumbnailUrl').value = videoData.thumbnailUrl;
+    document.getElementById('videoYoutubeId').value = videoData.id;
+    document.getElementById('newVideoForm').style.display = 'block';
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    
 
+    // clear form
+
+    // When user submits the 'New Video' button
     document.getElementById('newVideo').onclick = () => {
         
-        const videoId = document.getElementById('videoInputLink').value;
-        
-        getVideoAndUpdatePage(videoId);
+        // Gets YouTube URL from the form input
+        const youtubeUrl = document.getElementById('videoInputLink').value;
+        processYoutubeUrl(youtubeUrl);
 
-        document.getElementById('newVideoForm').style.display = 'block';
     }
 
 })
