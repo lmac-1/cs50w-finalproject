5c0dde9d19634b6c3a8997e1c46e40862ef174a2
lmac-1 <macartney.lucy@gmail.com>
2021-08-06 17:18:09 -0500

    Changing JS to build video HTML

--- danceapp/static/danceapp/videos.js
+++ danceapp/static/danceapp/videos.js
@@ -19,13 +19,36 @@ async function getVideosHTML() {
 
     videos.forEach(video => {
 
+        // Create link element
         const videoLink = document.createElement('a');
         videoLink.className = 'text-decoration-none';
         // TODO change these links to be less hardcoded
         videoLink.href=`/video/${video.id}`;
-        videoLink.innerHTML = video.title;
-        
+        videoLink.id = `link_for_${video.id}`
         document.getElementById("videoContainer").appendChild(videoLink);
+
+        // Create enclosing DIV
+        const videoDiv = document.createElement('div');
+        videoDiv.className = 'card';
+        videoDiv.style = 'width: 18rem;';
+        videoDiv.id = `video_container_${video.id}`;
+        videoLink.appendChild(videoDiv);
+
+        // Create image thumbnail
+        const videoThumbnail = document.createElement('img');
+        videoThumbnail.className = 'card-img-top';
+        videoThumbnail.alt = `Image for ${video.title}`;
+        videoThumbnail.src = video.thumbnail_url;
+        videoDiv.appendChild(videoThumbnail);
+
+        const videoBodyDiv = document.createElement('div');
+        videoBodyDiv.className = 'card-body';
+        videoDiv.appendChild(videoBodyDiv);
+
+        const videoLinkText = document.createElement('p');
+        videoLinkText.className = 'card-text';
+        videoLinkText.innerHTML = video.title;
+        videoBodyDiv.appendChild(videoLinkText);
     });
 
 }
