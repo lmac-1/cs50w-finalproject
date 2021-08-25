f7dc20cfae43e922eb795b11e5387e27e5f1c491
lmac-1 <macartney.lucy@gmail.com>
2021-08-24 19:05:10 -0500

    Notifications page for medium and below devices

--- danceapp/static/danceapp/notifications.js
+++ danceapp/static/danceapp/notifications.js
@@ -1,7 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
+    
     document.getElementById('notification-toggle').addEventListener("click", function() {
-        let notificationDiv = document.getElementById('notification-section');
-
-        notificationDiv.classList.toggle('d-none');
+        // Desktop (large and above) will show the notifications
+        if (window.innerWidth >= 992) {
+            let notificationDiv = document.getElementById('notification-section');
+            notificationDiv.classList.toggle('d-none');
+        } 
+        // Medium and below devices will take the user to the notifications page
+        else {
+            location.href = "/notifications";
+        }
+        
+        
     })
 });
