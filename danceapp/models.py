263a32c8ab0e5733503f71a6a7bf3a2cd38a4468
lmac-1 <macartney.lucy@gmail.com>
2021-09-30 16:29:20 -0500

    Tidying models

--- danceapp/models.py
+++ danceapp/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
 class User(AbstractUser):
     pass
     # Flag that determines whether the user is a student or teacher
